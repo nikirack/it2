3a1cf32685d1f9b25169c36f8463b44804713d39
Nikolai <109730887+Nikirack@users.noreply.github.com>
2026-09-13 14:37:21 +0200

    rps en linje men bedre

--- grunnlegende-python/rps-mini.py
+++ grunnlegende-python/rps-mini.py
@@ -16,3 +16,21 @@
 
 # import random; winning: dict[str,str] = {"stein":"saks","saks":"papir","papir":"stein"}
 # for _ in range(3): user = input("stein, saks eller papir: "); bot = random.choice(["stein","saks","papir"]); print("bruker vant" if winning[user] == bot else "uavgjort" if user == bot else "bot vant")
+
+
+# _ = [
+#     globals().update({"random":__import__("random"),"winning":{"stein":"saks","saks":"papir","papir":"stein"}}),
+#     [(
+#         globals().update({"user": input("stein, saks eller papir: ")}),
+#         globals().update({"bot": random.choice(["stein", "saks", "papir"])}),
+#         print(
+#             "bruker vant" if winning[user] == bot
+#             else "uavgjort" if user == bot
+#             else "bot vant"
+#         )
+#     )
+#     for _ in range(3)]
+# ]
+
+# one line no semicolon 
+_ = [ globals().update({"random":__import__("random"),"winning":{"stein":"saks","saks":"papir","papir":"stein"}}), [( globals().update({"user": input("stein, saks eller papir: ")}), globals().update({"bot": random.choice(["stein", "saks", "papir"])}), print( "bruker vant" if winning[user] == bot else "uavgjort" if user == bot else "bot vant" )) for _ in range(3)]]
